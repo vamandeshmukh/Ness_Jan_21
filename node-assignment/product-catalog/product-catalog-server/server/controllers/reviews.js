0908ff1cfb3a820b9596bc03d4bc786e2cebbb69
Vaman Deshmukh <vamandeshmukh@gmail.com>
2021-01-15 16:22:45 +0530

    Commit
    Commit

--- node-assignment/product-catalog/product-catalog-server/server/controllers/reviews.js
+++
@@ -1,5 +0,0 @@
-const getController = require( './getController' );
-const mongoose = require( 'mongoose' );
-const Review = mongoose.model( 'Review' );
-
-module.exports = getController( Review );
--- node-assignment/product-catalog/product-catalog-server/server/routes/authentication.js
+++
@@ -1,9 +0,0 @@
-const express = require( 'express' );
-const AuthenticationCtrl = require( '../controllers/authentication' );
-
-const router = express.Router();
-
-router.post( '/login', AuthenticationCtrl.login );
-router.post( '/logout', AuthenticationCtrl.logout );
-
-module.exports = router;
